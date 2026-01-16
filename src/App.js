d61e1a970d29893c94f165bc1e4ca9d5060ed52d
mukes <mukes@example.com>
2026-01-16 09:37:45 +0530

    Prevent counter from going below 0

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
   const [count, setCount] = useState(0);
 
   const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
   const reset = () => setCount(0);
 
   return (
